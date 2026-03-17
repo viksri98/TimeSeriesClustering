a2e1b2f6e49900e7d7ec086e8b4e995c33e124db
viksri98 <viksri98@gmail.com>
2026-03-16 20:08:40 -0700

    randomnet plotting

--- RandomNet/randomnet.R
+++ RandomNet/randomnet.R
@@ -1,9 +1,30 @@
 #RandomNet Output:
-clusters <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+clusters <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) + 1
 train <- read.table("Coffee_TRAIN.tsv")
 test <- read.table("Coffee_TEST.tsv")
 df <- rbind(train, test)
-df$cluster <- clusters + 1
+
+y <- df[,1]
+X <- df[,-1]
+library(ggplot2)
+
+df_long <- data.frame(
+		series_id = rep(seq_len(nrow(X)), each=ncol(X)),
+		class = rep(y, each=ncol(X)),
+		time = rep(seq_len(ncol(X)), times=nrow(X)),
+		value = as.vector(t(as.matrix(X))),
+		cluster = rep(clusters, each=ncol(X))
+)
+
+ggplot(df_long, aes(x=time, y=value, group=series_id, color=cluster)) +
+		geom_line(linewidth=0.25) +
+		facet_wrap(~ class, ncol=2) +
+		labs(
+			 title="Coffee Beans by Class",
+			 x = "Time",
+			 y = "Signal"
+		) +
+		theme_minimal(base_size=10, base_family="serif")
 
 df1 <- df[df$cluster==1,]
 df2 <- df[df$cluster==2,]
